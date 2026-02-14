5a99aa65ee5f6c172ca18410af6c3116f2f8be29
Jim Greer <jbgreer@gmail.com>
2026-02-14 15:13:17 -0600

    removed visual studio domains from FW rules

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -66,7 +66,8 @@ while read -r cidr; do
   ipset add allowed-domains "$cidr"
 done < <(echo "$gh_ranges" | jq -r '(.web + .api + .git)[]' | aggregate -q)
 
-# Resolve and add other allowed domains
+#"" Resolve and add other allowed domains
+#
 for domain in \
   "registry.npmjs.org" \
   "rubygems.org" \
@@ -74,10 +75,7 @@ for domain in \
   "api.anthropic.com" \
   "sentry.io" \
   "statsig.anthropic.com" \
-  "statsig.com" \
-  "marketplace.visualstudio.com" \
-  "vscode.blob.core.windows.net" \
-  "update.code.visualstudio.com"; do
+  "statsig.com"; do
   echo "Resolving $domain..."
   ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
   if [ -z "$ips" ]; then
